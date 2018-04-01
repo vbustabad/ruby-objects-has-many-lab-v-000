e77672fb68c9750f6a8e022d5482a2bde4fc63d0
Veronica Bustabad <vbustabad@gmail.com>
2018-04-01 00:36:25 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -8,10 +8,17 @@ class Post
 
   def author_name
     if self.author == true
-      self.author_name = @author.name
+      self.author_name = author.name
     else
       return nil
     end
   end
+
+  describe "#author_name" do
+    it "knows the name of its author" do 
+      sophie = Author.new("Sophie")
+      post.author = sophie
+      expect(post.author_name).to eq("Sophie")
+    end
 
 end
